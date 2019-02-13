6bfe3456f0b31201ff2a55b32f721e4efd9b755d
zhengyi <zhengyi@fishcredit.net>
2019-02-13 17:05:28 +0800

    no message

---
+++ oa-api/src/main/java/com/zbjdl/oa/service/UserInfoService.java
@@ -0,0 +1,25 @@
+/*
+ * Powered By zbjdl-code-generator
+ * Web Site: http://www.zbjdl.com
+ * Since 2018 - 2022
+ */
+
+package com.zbjdl.oa.service;
+
+import java.util.List;
+
+import com.zbjdl.oa.dto.UserInfoDto;
+
+public interface UserInfoService {
+	
+	Integer save(UserInfoDto userInfoSaveReqDto);
+
+	Integer update(UserInfoDto userInfoSaveReqDto);
+
+	UserInfoDto selectById(Long id);
+
+	List<UserInfoDto> findList(UserInfoDto userInfoDto);
+
+	UserInfoDto login(String username, String password);
+
+}
